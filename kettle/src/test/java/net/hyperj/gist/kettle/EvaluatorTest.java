5712217904db90154f38e986925d0e5f273c12cb
HyperJ <hyperj@qq.com>
2018-12-16 23:16:42 +0800

    update

--- kettle/src/test/java/net/hyperj/gist/kettle/EvaluatorTest.java
+++ kettle/src/test/java/net/hyperj/gist/kettle/EvaluatorTest.java
@@ -1,5 +1,6 @@
 package net.hyperj.gist.kettle;
 
+import org.junit.Test;
 import org.pentaho.di.core.row.ValueMetaInterface;
 import org.pentaho.di.core.util.DateDetector;
 import org.pentaho.di.core.util.StringEvaluationResult;
@@ -8,14 +9,13 @@ import org.pentaho.di.core.util.StringEvaluator;
 import java.util.Arrays;
 import java.util.List;
 
-public class Evaluator {
-    public static void main(String[] args) {
-        // String Evaluator
+public class EvaluatorTest {
+
+    @Test
+    public void evaluator() {
         StringEvaluator evaluator = new StringEvaluator();
         List<String> strings = Arrays.asList("2018-11-11");
-        for (String string : strings) {
-            evaluator.evaluateString(string);
-        }
+        strings.forEach(evaluator::evaluateString);
         StringEvaluationResult advicedResult = evaluator.getAdvicedResult();
         ValueMetaInterface conversionMeta = advicedResult.getConversionMeta();
         System.out.println(ValueMetaInterface.getTypeDescription(conversionMeta.getType()));
