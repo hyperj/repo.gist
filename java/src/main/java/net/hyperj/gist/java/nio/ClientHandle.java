c35b49b60dbf9fba0f65a7b7a48293d1ea5d0cec
HyperJ <hyperj@qq.com>
2019-01-11 23:35:43 +0800

    update

---
+++ java/src/main/java/net/hyperj/gist/java/nio/ClientHandle.java
@@ -0,0 +1,119 @@
+package net.hyperj.gist.java.nio;
+
+import net.hyperj.gist.common.dict.StringDict;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.SocketChannel;
+import java.util.Iterator;
+import java.util.Set;
+
+public class ClientHandle implements Runnable {
+    private String host;
+    private int port;
+    private Selector selector;
+    private SocketChannel socketChannel;
+    private volatile boolean started;
+
+    public ClientHandle(String ip, int port) {
+        this.host = ip;
+        this.port = port;
+        try {
+            selector = Selector.open();
+            socketChannel = SocketChannel.open();
+            socketChannel.configureBlocking(false);
+            started = true;
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+    }
+
+    public void stop() {
+        started = false;
+    }
+
+    @Override
+    public void run() {
+        try {
+            doConnect();
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+        while (started) {
+            try {
+                selector.select(1000);
+                Set<SelectionKey> keys = selector.selectedKeys();
+                Iterator<SelectionKey> it = keys.iterator();
+                SelectionKey key = null;
+                while (it.hasNext()) {
+                    key = it.next();
+                    it.remove();
+                    try {
+                        handleInput(key);
+                    } catch (Exception e) {
+                        if (key != null) {
+                            key.cancel();
+                            if (key.channel() != null) {
+                                key.channel().close();
+                            }
+                        }
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+                System.exit(1);
+            }
+        }
+        if (selector != null) {
+            try {
+                selector.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void handleInput(SelectionKey key) throws IOException {
+        if (key.isValid()) {
+            SocketChannel sc = (SocketChannel) key.channel();
+            if (key.isConnectable()) {
+                if (sc.finishConnect()) ;
+                else System.exit(1);
+            }
+            if (key.isReadable()) {
+                ByteBuffer buffer = ByteBuffer.allocate(1024);
+                int readBytes = sc.read(buffer);
+                if (readBytes > 0) {
+                    buffer.flip();
+                    byte[] bytes = new byte[buffer.remaining()];
+                    buffer.get(bytes);
+                    String result = new String(bytes, StringDict.CHARSET_UTF8);
+                    System.out.println("Response Msg: " + result);
+                }
+                else if (readBytes < 0) {
+                    key.cancel();
+                    sc.close();
+                }
+            }
+        }
+    }
+
+    private void doWrite(SocketChannel channel, String request) throws IOException {
+        ServerHandle.write(channel, request);
+    }
+
+    private void doConnect() throws IOException {
+        if (socketChannel.connect(new InetSocketAddress(host, port))) ;
+        else socketChannel.register(selector, SelectionKey.OP_CONNECT);
+    }
+
+    public void sendMsg(String msg) throws Exception {
+        socketChannel.register(selector, SelectionKey.OP_READ);
+        doWrite(socketChannel, msg);
+    }
+}
---
+++ java/src/main/java/net/hyperj/gist/java/nio/Test.java
@@ -0,0 +1,13 @@
+package net.hyperj.gist.java.nio;
+
+import java.util.Scanner;
+
+public class Test {
+
+    public static void main(String[] args) throws Exception {
+        Server.start();
+        Thread.sleep(1000);
+        Client.start();
+        while (Client.sendMsg(new Scanner(System.in).nextLine())) ;
+    }
+}
