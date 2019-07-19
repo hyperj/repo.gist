ec2af4e5101f59119ac23044b134f11b9b6149f5
hyperj <hyperj@qq.com>
2019-07-19 10:06:18 +0800

    leetcode: 66. Plus One

---
+++ leetcode/src/main/java/net/hyperj/gist/leetcode/Q66PlusOne.java
@@ -0,0 +1,23 @@
+package net.hyperj.gist.leetcode;
+
+import java.util.Arrays;
+
+public class Q66PlusOne {
+    public int[] plusOne(int[] digits) {
+        for (int i = digits.length - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i] += 1;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+        digits = new int[digits.length + 1];
+        digits[0] = 1;
+        return digits;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(new Q66PlusOne().plusOne(new int[]{9, 9, 9, 9})));
+    }
+
+}
