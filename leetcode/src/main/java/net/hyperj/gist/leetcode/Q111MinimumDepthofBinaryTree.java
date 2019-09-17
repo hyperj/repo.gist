fe5d05b63e45ef3665516ab0ee271bdc9a649dc0
hyperj <hyperj@qq.com>
2019-09-17 23:04:29 +0800

    leetcode: 111. Minimum Depth of Binary Tree

---
+++ leetcode/src/main/java/net/hyperj/gist/leetcode/Q111MinimumDepthofBinaryTree.java
@@ -0,0 +1,24 @@
+package net.hyperj.gist.leetcode;
+
+public class Q111MinimumDepthofBinaryTree {
+
+    public int minDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        if (root.left == null) {
+            return minDepth(root.right) + 1;
+        }
+        if (root.right == null) {
+            return minDepth(root.left) + 1;
+        }
+        return Math.min(minDepth(root.left), minDepth(root.right)) + 1;
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.left.right = new TreeNode(3);
+        System.out.println(new Q111MinimumDepthofBinaryTree().minDepth(root));
+    }
+}
