4ac2d18df9dd9907b0f6bf6b4489d33539beefa5
hyperj <hyperj@qq.com>
2020-07-12 23:10:41 +0800

    leetcode: 581. Shortest Unsorted Continuous Subarray

---
+++ leetcode/src/main/java/net/hyperj/gist/leetcode/Q581ShortestUnsortedContinuousSubarray.java
@@ -0,0 +1,20 @@
+package net.hyperj.gist.leetcode;
+
+import java.util.Arrays;
+
+public class Q581ShortestUnsortedContinuousSubarray {
+
+    public int findUnsortedSubarray(int[] nums) {
+        int[] snums = nums.clone();
+        Arrays.sort(snums);
+        int start = snums.length, end = 0;
+        for (int i = 0; i < snums.length; i++) {
+            if (snums[i] != nums[i]) {
+                start = Math.min(start, i);
+                end = Math.max(end, i);
+            }
+        }
+        return (end - start >= 0 ? end - start + 1 : 0);
+    }
+
+}
