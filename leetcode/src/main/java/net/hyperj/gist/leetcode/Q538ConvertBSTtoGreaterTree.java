86c276013a790a6b928a796b972587424cc6aa34
hyperj <hyperj@qq.com>
2020-07-14 00:39:48 +0800

    leetcode: 538. Convert BST to Greater Tree

---
+++ leetcode/src/main/java/net/hyperj/gist/leetcode/Q538ConvertBSTtoGreaterTree.java
@@ -0,0 +1,17 @@
+package net.hyperj.gist.leetcode;
+
+public class Q538ConvertBSTtoGreaterTree {
+
+    private int sum = 0;
+
+    public TreeNode convertBST(TreeNode root) {
+        if (root != null) {
+            convertBST(root.right);
+            sum += root.val;
+            root.val = sum;
+            convertBST(root.left);
+        }
+        return root;
+    }
+
+}
