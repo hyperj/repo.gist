5a92ef26a8b3ca1ac3b95aeafb6371263819c7bc
hyperj <hyperj@qq.com>
2020-07-15 22:49:31 +0800

    leetcode: 461. Hamming Distance

---
+++ leetcode/src/main/java/net/hyperj/gist/leetcode/Q461HammingDistance.java
@@ -0,0 +1,16 @@
+package net.hyperj.gist.leetcode;
+
+public class Q461HammingDistance {
+
+    public int hammingDistance(int x, int y) {
+        int xor = x ^ y;
+        int distance = 0;
+        while (xor != 0) {
+            if (xor % 2 == 1) {
+                distance += 1;
+            }
+            xor = xor >> 1;
+        }
+        return distance;
+    }
+}
